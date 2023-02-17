54b1177c96613e3a89d61a562c28531d2f2159e9
Rajkumar7878 <106055610+Rajkumar7878@users.noreply.github.com>
2023-02-17 09:59:23 +0530

    Added addition program

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,16 @@
-public class Helloworld {
+public class Helloworld
+{
+  int addnumber(int a, int b)
+  {
+    int c=a+b;
+  return c;
+}
+
+
   public static void main(String[] args) {
+     Helloworld obj=New Helloworld();
+       System.out.println("addition is "+obj.addnumber(3,7);
     System.out.println("Hello world!, new java program");
   }
+
 }
